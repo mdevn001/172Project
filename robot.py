d00a5873c63a50a60241c2b921e9dc19b9c9259c
Edward Miranda <emira035@ucr.edu>
2023-05-05 23:28:34 -0700

    finished

--- robot.py
+++ robot.py
@@ -2,15 +2,19 @@ import os
 import sys
 import requests
 import queue
+import copy
+from simhash import Simhash
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from urllib.parse import urlsplit
 from threading import Thread
+from multiprocessing import Pool, Value
+import time
 
 
 restricted_extensions= ['.pdf','.jpeg','.jpg','.doc','.txt','.rtf','.wpd','.avi','mpg','.wmv','.vob','.flv','.3gp',
                         '.mpg3','.wma','.wav','.mid''.java','.py','.cs','.php','.swift','.vb','.rss','.css','.cer',
-                        '.asp','.aspx','.bmp','.tif','.gif','.png']
+                        '.asp','.aspx','.bmp','.tif','.gif','.png','.js$','.js','.svg']
 
 def extension_valid(string):
     
@@ -76,9 +80,13 @@ def get_children(soup,parent,restricted_domains,base_seeds):
 
 # simple function for encoding ambersand can include more characters later
 def encode_reserved(str):
-    reserved_characters  = ['&']
+    reserved_characters  = ['&',' ']
     for char in reserved_characters:
-        str = str.replace(char,'%26')
+        if(char == '&'):
+            str = str.replace(char,'%26')
+        else:
+            if(char ==' '):
+                str =  str.replace(char,'%20')
     return str
 
 #start stripping out the url so we can only get html
@@ -100,44 +108,48 @@ def valid_url(child_url):
                 return True
     return False
 
-def getrestricted_domains(seed_nodes):
+def getrestricted_domains(seeds):
     
     restricted_domains = []
+    seeds_cpy =  copy.deepcopy(seeds)
     
-    for node in seed_nodes:
-        robots_url = node.url+'/robots.txt'
+    for seed in seeds_cpy:
+        robots_url = urljoin(seed,'/robots.txt')
         result = requests.get(robots_url)
         status_code = result.status_code
         if status_code == 200: # OK
-            result = result.text;
+            result = result.text
             for line in result.split("\n"):
                 if line.startswith('Disallow:'):
                     split = line.split(' ', maxsplit=1)
-                    domain = split[1].strip()      
+                    domain = split[1].strip()
+                    domain = domain.replace('*','') # some robots use * to dictate any
                     if  domain not in restricted_domains:
                         restricted_domains.append(line)
-        else:
-            print("test")
+                        
+                    #adds any relative paths to our seeds
+                if line.startswith('Allow:'):
+                    split = line.split(' ', maxsplit=1)
+                    domain = split[1].strip()
+                    domain = domain.replace('*','') # some robots use * to dictate any
+                    crawlable_url = urljoin(seed,domain)
+                    if(crawlable_url not in seeds and domain  != '/' and extension_valid(crawlable_url)):
+                        print("added: "+ crawlable_url+ " to list of crawlable paths.")
+                        seeds.append(crawlable_url)
     return restricted_domains
-
-def task():
-    print("DONE!")
 
 def getseedsFromFile(filename):
     urls =  open(filename).readlines()
-    seed_nodes = []
+    seeds = []
     for url in urls:
-        n1 = Node(url)
-        seed_nodes.append(n1)
-        
-    return seed_nodes
+        seeds.append(url)
+    return seeds
 
 def createDirectory(directory_name):
     exists = os.path.exists(directory_name)
     if  not exists:
         os.makedirs(directory_name)
 
-seed_nodes = getseedsFromFile(sys.argv[1])
 max_pages = int(sys.argv[2])
 hops = int(sys.argv[3])
 output_dir = sys.argv[4]
@@ -145,35 +157,48 @@ output_dir = sys.argv[4]
 #create directory
 createDirectory(output_dir)
 
-no_threads = 5
-page_request_delay = 5000 # set to 1000ms = every second or 60 request a min
+page_request_delay = 1 # every 20 seconds send 5 requests or 15 requests a min
 
-restricted_domains = getrestricted_domains(seed_nodes)
+seeds = getseedsFromFile(sys.argv[1])
+seed_nodes = []
+
+restricted_domains = getrestricted_domains(seeds)
+
+for seed in seeds:
+    seed = seed.strip()
+    n1 = Node(seed)
+    seed_nodes.append(n1)
 
 #setup the queue/frontier
 q = queue.Queue()
 visited =[]
+hashes = [] #hashed documents for comparing similiarity
+
 
 #add all the seeds to the queue
 for node in seed_nodes:
-    pass
-    #q.put(node)
+    q.put(node)
 
-found_pages =0
-pages_maximum = 1000# just temporary we will use the preset 100000 later
+def save_html_page(soup,output_dir):
+    result = urlsplit(node.url)
+    filename =result.hostname+result.path
+    filename =output_dir+'/'+filename.replace('/','_')+".html"
+    file = open(filename,'w+',encoding='utf-8')
+    file.write(str(soup))
+    file.close()
+ 
+downloaded_pages = 0  
+max_pages = 10000
 
-#while queue is not empty
-#while not q.empty() and (found_pages < pages_maximum):
-   # node = q.get()    
-    
-    #proceed only if the node we found not visited or 5  nodes deep from  seed nodes
-    #if(node.url not in visited and node.depth < 5):
+while (not q.empty() and downloaded_pages < max_pages):
+    node = q.get()   
+     #proceed only if the node we found not visited or 5  nodes deep from  seed nodes
+    if(node.url not in visited and node.depth < hops):
          #add it to visited list /improve this later for checking similarity etc??
-       # visited.append(node.url)
+        visited.append(node.url)
+        result = requests.get(node.url)
         
-       # result = requests.get(node.url)
-        
-       # if(result.status_code == 200): #ok
+        if(result.status_code == 200): #ok
            
             htmldoc = result.text
             soup = BeautifulSoup(htmldoc,'html.parser')
@@ -183,8 +208,13 @@ pages_maximum = 1000# just temporary we will use the preset 100000 later
             for child_node in child_nodes:
                 q.put(child_node)
         
-            #found_pages = found_pages+1
-        
-           # print("visited: "+ node.url + " depth: " + str(node.depth))
-    #newThread = Thread(target=task)
-    #newThread.start()
+            save_html_page(soup,output_dir)
+            downloaded_pages = downloaded_pages +1
+            print("visited: "+ node.url + " depth: " + str(node.depth))
+            time.sleep(page_request_delay)
+
+if q.empty():
+    print("finished all links crawled in queue.")
+else:
+     if(downloaded_pages>=max_pages):
+         print("we have hit the document download limit : " + max_pages +"pages")
